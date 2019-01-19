ed637b1d7672fcf687cbd7a0d69168db2f1004af
Alexandr Kim <alexandr.kim@e-azbuka.ru>
2019-01-19 23:59:06 +0300

    update massFollow

--- massFollow/massFollow.service.js
+++ massFollow/massFollow.service.js
@@ -1,15 +1,29 @@
 const User = require('../user/user.model');
+const Follower = require('../follower/follower.model');
 
 class MassFollowService {
   constructor() {}
 
   async createWork(data) {
-    const activeUsers = await User.find().exec();
-    console.log(activeUsers)
+    const activeUsers = await User.find({
+      is_active: true
+    }).exec();
+
+    if (!activeUsers.length) {
+      return 'no active users'
+    }
+
+    const followers = await Follower.find({
+      followerUser: activeUsers[0]._id
+    }).exec();
 
 
+    // followers.forEach((follower) => {
+    //   await follower(follower.id)
+    // })
 
-
+    console.log(followers, '----')
+    return followers
     // return new Follower(data).save();
   }
 
